40a58769296e722f50364d187e98e420044f14e9
Chernecky <vetal-chernecky@mail.ru>
2020-12-08 17:51:22 +0300

    skelet4

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=300)
     date = models.DateTimeField()
-    text = models.CharField()
+    text = models.TextField()
     image = models.ImageField(upload_to='event_images/')
--- s6/urls.py
+++ s6/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-
+import event.views
 from s6 import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', event.views.home, name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
