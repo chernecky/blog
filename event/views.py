48c40e16c928a4b283a6e8cd042fdebec8989a6c
Chernecky <vetal-chernecky@mail.ru>
2020-12-11 10:32:53 +0300

    add home.html

--- event/views.py
+++ event/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Event
 
 def home(request):
+#    event = Event.objects
     return render(request, 'event/home.html')
+
+    # 'event/home.html', {'event': event}
